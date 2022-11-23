3dd5abf30e0804e70c5ccf1d727e7cb646abeaf5
102sae <95170874+102sae@users.noreply.github.com>
2022-11-23 22:55:37 +0900

    calender add

---
+++ app/src/main/java/com/course/mydietapp/CalendarActivity.java
@@ -0,0 +1,25 @@
+package com.course.mydietapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.format.ArrayWeekDayFormatter;
+import com.prolificinteractive.materialcalendarview.format.MonthArrayTitleFormatter;
+
+public class CalendarActivity extends AppCompatActivity {
+    private MaterialCalendarView calendarView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+
+        calendarView = findViewById(R.id.calendarview);
+
+        calendarView.setTitleFormatter(new MonthArrayTitleFormatter(getResources().getTextArray(R.array.custom_months)));
+        calendarView.setWeekDayFormatter(new ArrayWeekDayFormatter(getResources().getTextArray(R.array.custom_weekdays)));
+    }
+
+}
